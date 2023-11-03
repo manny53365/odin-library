4bf8a861c23d72763c99bfdf4fa7d7935b9c26cb
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-11-03 17:46:56 +0530

    refactored code as the displayAddBookModal function was too long

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// defining a place to store all of my books
-
 const myLibrary = [
     {
         title: "Test Book",
@@ -33,34 +31,35 @@ const myLibrary = [
     }
 ];
 
-// Add book button
 const addBookBtn = document.getElementById('add-book-btn');
 
-// Test add book button
-// addBookBtn.addEventListener('click', () => {console.log('Clicked')});
-
-// Sample constructor function
-
-// function Book(title, author, pages, read){
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// };
-
-// console.log(typeof(Book));
-
-function Book(title,pages,author,read){
-    // Add event input from the DOM
-}
-
-const displayAddBookModal = () => {
-    const booksContainer = document.getElementById('book-container');
-  
+const createAddBookDialog = () => {
     const dialog = document.createElement('dialog');
     dialog.id = 'addBookDialog';
-  
+
     const form = document.createElement('form');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const author = event.target.author.value;
+      const pages = event.target.pages.value;
+      const read = event.target.read.checked;
+
+      const newBook = {
+        title: title,
+        author: author,
+        pages: pages,
+        read: read,
+      };
+
+      addBookToLibrary(newBook);
+
+      dialog.close();
+
+      event.target.reset();
+
+      addBooksToDom();
+    });
   
     const titleLabel = document.createElement('label');
     titleLabel.textContent = 'Title: ';
@@ -84,39 +83,19 @@ const displayAddBookModal = () => {
     readLabel.textContent = 'Read: ';
     const readInput = document.createElement('input');
     readInput.type = 'checkbox';
+    readInput.name = 'read';
   
     const submitButton = document.createElement('button');
-    submitButton.textContent = 'Submit';
+    submitButton.textContent = 'Add book';
     submitButton.type = 'submit';
-  
-    submitButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      const title = titleInput.value;
-      const author = authorInput.value;
-      const pages = pagesInput.value;
-      const read = readInput.checked;
 
-      const newBook = {
-        title: title,
-        author: author,
-        pages: pages,
-        read: read
-      };
-
-      myLibrary.push(newBook);
-
-      dialog.close();
-
-      addBooksToDom();
-    });
-  
     const cancelButton = document.createElement('button');
     cancelButton.textContent = 'Cancel';
     cancelButton.type = 'button';
     cancelButton.addEventListener('click', () => {
       dialog.close();
     });
-
+  
     form.appendChild(titleLabel);
     form.appendChild(titleInput);
     form.appendChild(document.createElement('br'));
@@ -133,19 +112,22 @@ const displayAddBookModal = () => {
     form.appendChild(cancelButton);
 
     dialog.appendChild(form);
+  
+    return dialog;
+  };
+
+const displayAddBookModal = () => {
+    const booksContainer = document.getElementById('book-container');
+    const dialog = createAddBookDialog();
 
     booksContainer.appendChild(dialog);
-
     dialog.showModal();
 };
-  
-  
 
 addBookBtn.addEventListener('click', displayAddBookModal);
 
-const addBookToLibrary = () => {
-    // This will be called by the book function with the args from Book passed in
-    // Following the creation of the obj, then append the data to myLibrary array
+const addBookToLibrary = (book) => {
+    myLibrary.push(book);
 }
 
 const addBooksToDom = () => {
@@ -175,5 +157,3 @@ const addBooksToDom = () => {
       booksArr.appendChild(bookCard);
     });
 };
-
-addBooksToDom();
